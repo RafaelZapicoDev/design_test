d79d0f611000e7312240fe3b2f8fda91b84c0209
rafael <rafaelzapico.ti@gmail.com>
2025-03-12 15:51:32 -0300

    layout fixes

--- lib/bottombar.dart
+++ lib/bottombar.dart
@@ -24,7 +24,7 @@ class BottomBar extends StatelessWidget {
                         style: TextStyle(
                             color: theme['foreground1'],
                             decoration: TextDecoration.none,
-                            fontSize: 30),
+                            fontSize: 25),
                       ),
                     ],
                   )),
@@ -33,6 +33,33 @@ class BottomBar extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.import_export_sharp,
+                          color: theme['foreground1'],
+                          size: 25,
+                        ),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Text(
+                          "Importar ou Exportar",
+                          style: TextStyle(
+                              color: theme['foreground1'],
+                              decoration: TextDecoration.none,
+                              fontSize: 15),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 15),
+                      child: VerticalDivider(
+                        color: theme['foreground4'],
+                        thickness: 2,
+                      ),
+                    ),
                     Row(
                       children: [
                         Icon(
